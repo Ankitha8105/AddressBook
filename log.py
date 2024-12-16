1bb2fff1e726b32f03107f0f8ac7fba686d54c6c
Ankitha8105 <ankithabl2001@gmail.com>
2024-12-16 10:59:31 +0530

    [Ankitha] Add:Logger python file to log the messages

---
+++ log.py
@@ -0,0 +1,21 @@
+import logging
+
+def logger_init(name):
+    
+    logger = logging.getLogger(name)
+    
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(acstime)s,%(name)s,%(levelname)s,%(message)s")
+    
+    file_handler = logging.FileHandler("all.log")
+    file_handler.setFormatter(formatter)
+    
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    
+    return logger
+    
+    
