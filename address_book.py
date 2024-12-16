e7c62fc92b6649883e21146ee07d2534a8af1c16
Ankitha8105 <ankithabl2001@gmail.com>
2024-12-16 14:56:28 +0530

    [Ankitha] Modified:Python program to add New Contact address

--- address_book.py
+++ address_book.py
@@ -10,142 +10,82 @@ import log
 log = log.logger_init("Address Book")
 
 class Contact:
-    
-    def __init__(self,first_name,last_name,address,city,state,zip,phone_num,email):
-        """
-            Description:
-                Initializes an Contact object with persons details
-        """
+    def __init__(self, first_name, last_name, address, city, state, zip_code, phone, email):
         self.first_name = first_name
         self.last_name = last_name
         self.address = address
         self.city = city
         self.state = state
-        self.zip = zip
-        self.phone = phone_num
+        self.zip_code = zip_code
+        self.phone = phone
         self.email = email
-        
-    def __str__(self):
-        """
-        Description:
-            Display Object in string representation
-        """
-        return (f"{self.first_name} {self.last_name},{self.address},{self.city},{self.state},{self.zip},{self.phone},{self.email}")
 
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}, {self.address}, {self.city}, {self.state}, {self.zip_code}, {self.phone}, {self.email}")
 
 class AddressBook:
     def __init__(self):
-        """
-            Description:
-                Initializes an Contact list 
-        """
         self.contacts = {}
-        
-    def add_contacts(self,contact):
-        """
-            Description:
-                Function to add new contact to contact book
-            Parameters:
-                contact = contact to add
-            Return:
-                It returns contact is added or not
-        """
-        contact_key = f"{contact.first_name,contact.last_name}"
 
-        if contact_key not in self.contacts:
-            self.contacts[contact_key] = contact
-            log.info(self.contacts.keys())
-            log.info(f"Contact {contact_key} added successfully.")
+    def add_contact(self, contact):
+        key = f"{contact.first_name} {contact.last_name}"
+        if key not in self.contacts:
+            self.contacts[key] = contact
+            log.info(self.contacts.items())
+            log.info(f"Contact {key} added successfully.")
         else:
-            log.info(f"Contact {contact_key} already exists in the address book.")
+            log.info(f"Contact {key} already exists in the address book.")
+
+    def display_contacts(self):
+        if self.contacts:
+            for contact in self.contacts.values():
+                log.info(contact)
+        else:
+            log.info("No contacts in the address book.")
 
     
 class AddressBookMain:
     def __init__(self):
-        """
-            Description:
-                Initializes address book for list of contacts
-        """
         self.address_book = AddressBook()
 
-    def dispaly(self):
-        """
-            Description:
-                Function is used to display welcome message 
-        """
-        print("Welcome to Address Book program in AddressBookMain class")
-    
-    def details_from_console(self):
-        """
-            Description:
-                Function is used to get the details from the user
-            Return:
-                It returns contact object
-        """
-        self.first_name = input("Enter First Name :")
-        try:
-            match = re.search(r'^[A-Z][A-Za-z]{4,}$',self.first_name)
-        except Exception:
-            log.error("Please Enter correct first name..")
+    def display(self):
+        print("Welcome to Address Book problem")
         
-        self.last_name = input("Enter Last name :")
-        try:
-            match = re.search(r'^[A-Z]\s?[A-Za-z]{2,}$',self.last_name)
-        except Exception:
-            log.error("Please Enter correct last name..")
-            
-        self.address = input("Enter your Address :")
-        try:
-            match = re.search(r'^\w*$',self.address)
-        except Exception:
-            log.error("Please Enter correct Address..")
-            
-        self.city = input("Enter your city :")
-        try:
-            match = re.search(r'^[A-Za-z]{4,}$',self.city)
-        except Exception:
-            log.error("Please Enter correct city..")
-        
-        self.state = input("Enter your state :")
-        try:
-            match = re.search(r'^[A-Za-z]{3,}$',self.state)
-        except Exception:
-            log.error("Please Enter correct State..")
-        self.zip = input("Enter your pin code :")
-        try:
-            match = re.search(r'^[1-9][0-9]{5}$',self.zip)
-        except Exception:
-            log.error("Please Enter correct pincode..")
-            
-        self.phone_num = input("Enter phone number :")
-        try:
-            match = re.search(r'^[0-9]{10}$',self.phone_num)
-        except Exception:
-            log.error("Please Enter correct pincode..")
-            
-        self.email = input("Enter your email address :")
-        try:
-            match = re.search('^\[a-zA-Z0-9. _%+-]+@\[a-zA-Z0-9. -]+\. \[a-zA-Z\]{2,}$',self.email)
-        except Exception:
-            log.error("Please enter correct email Id")
-            
-        contact = Contact(self.first_name,self.last_name,self.address,self.city,self.state,self.zip,self.phone_num,self.email)
-        self.address_book.add_contacts(contact)
-        
+    def add_contact_from_console(self):
+        print("Enter the following contact details:")
+
+        first_name = input("First Name: ")
+        last_name = input("Last Name: ")
+        address = input("Address: ")
+        city = input("City: ")
+        state = input("State: ")
+        zip_code = input("Zip Code: ")
+        phone = input("Phone Number: ")
+        email = input("Email: ")
+
+        contact = Contact(first_name, last_name, address, city, state, zip_code, phone, email)
+        self.address_book.add_contact(contact)
+
     def run(self):
-        n=1
-        while n<5:
-            print("------Address Book------")
-            print("1.Add New contact")
-            
-            choice =  int(input("Input your choice: "))
-            
-            if choice == 1:
-                self.details_from_console()
+        while True:
+            print("\n--- Address Book ---")
+            print("1. Add New Contact")
+            print("2. Display Contact")
+            print("3.Exit")
+    
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                self.add_contact_from_console()
+            elif choice == "2":
+                self.address_book.display_contacts()
+            elif choice =="3":
+                break
             else:
-                print("Invalid choice..")
-        n += 1        
+                print("Invalid choice. Please try again.")
+
+
 if __name__=="__main__":
     address_main = AddressBookMain()
-    address_main.dispaly()
+    address_main.display()
     address_main.run()
