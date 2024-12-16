35f2ee22d13a70ed48d05815cd3bff259492a76b
Ankitha8105 <ankithabl2001@gmail.com>
2024-12-16 16:14:35 +0530

    [Ankitha] Added:Python program to Refactor to add multiple address book to the system

--- address_book.py
+++ address_book.py
@@ -47,12 +47,12 @@ class AddressBook:
             self.contacts[key].phone = input("Enter new phone number: ")
             self.contacts[key].email = input("Enter new email: ")
             
-    def delete_contact(self,first_name,last_name):
+    def delete_contact(self, first_name, last_name):
         key = f"{first_name} {last_name}"
         if key in self.contacts:
             del self.contacts[key]
         else:
-            print(f"{key} is not present in contact book ")
+            print(f"{key} is not present in the contact book")
 
     def display_contacts(self):
         if self.contacts:
@@ -61,17 +61,34 @@ class AddressBook:
         else:
             log.info("No contacts in the address book.")
 
-    
 class AddressBookMain:
     def __init__(self):
-        self.address_book = AddressBook()
-        
-    def diaply(self):
+        self.address_books = {}
+
+    def display_menu(self):
         print("Welcome to Address Book Problem")
 
-    def add_contact_from_console(self):
-        print("Enter the following contact details:")
+    def create_address_book(self):
+        book_name = input("Enter the name for the new Address Book: ")
+        if book_name not in self.address_books:
+            self.address_books[book_name] = AddressBook()
+            log.info(f"New Address Book '{book_name}' created successfully.")
+        else:
+            log.info(f"Address Book '{book_name}' already exists.")
 
+    def select_address_book(self):
+        print("Available Address Books:")
+        for name in self.address_books:
+            print(f"- {name}")
+        book_name = input("Enter the name of the Address Book you want to manage: ")
+        if book_name in self.address_books:
+            return self.address_books[book_name]
+        else:
+            log.info(f"Address Book '{book_name}' does not exist.")
+            return None
+
+    def add_contact_from_console(self, address_book):
+        print("Enter the following contact details:")
         first_name = input("First Name: ")
         last_name = input("Last Name: ")
         address = input("Address: ")
@@ -82,49 +99,63 @@ class AddressBookMain:
         email = input("Email: ")
 
         contact = Contact(first_name, last_name, address, city, state, zip_code, phone, email)
-        self.address_book.add_contact(contact)
+        address_book.add_contact(contact)
 
-    def edit_contact_from_console(self):
-        print("Enter the Following Details: ")
-
+    def edit_contact_from_console(self, address_book):
+        print("Enter the following details to edit a contact: ")
         f_name = input("Enter first name: ")
         l_name = input("Enter last name: ")
 
-        self.address_book.edit_contact(f_name,l_name)
-        
-    def delete_contact_main(self):
-        f_name = input("Enter First Name:")
-        l_name  = input("Enter Last Name:")
-        
-        self.address_book.delete_contact(f_name,l_name)
-        
-    def run(self):
-        n = int(input("Enter Number of Contacts to Add :"))
-        i = 1
-        while i <= n:
-            print("\n--- Address Book ---")
-            print("1. Add New Contact")
-            print("2. Edit Contact")
-            print("3. Delete Contact")
-            print("6. Display Contact")
-            print("7.Exit")
-    
-            choice = input("Enter your choice: ")
+        address_book.edit_contact(f_name, l_name)
 
+    def delete_contact_main(self, address_book):
+        f_name = input("Enter First Name: ")
+        l_name = input("Enter Last Name: ")
+        address_book.delete_contact(f_name, l_name)
+
+    def run(self):
+        while True:
+            print("\n--- Main Menu ---")
+            print("1. Create New Address Book")
+            print("2. Select Address Book")
+            print("3. Exit")
+
+            choice = input("Enter your choice: ")
             if choice == "1":
-                self.add_contact_from_console()
+                self.create_address_book()
             elif choice == "2":
-                self.edit_contact_from_console()
+                selected_address_book = self.select_address_book()
+                if selected_address_book:
+                    self.manage_address_book(selected_address_book)
             elif choice == "3":
-                self.delete_contact_main()
-            elif choice == "6":
-                self.address_book.display_contacts()
-            elif choice =="7":
                 break
             else:
                 print("Invalid choice. Please try again.")
-            i += 1
 
-if __name__=="__main__":
+    def manage_address_book(self, address_book):
+        while True:
+            print("\n--- Address Book Menu ---")
+            print("1. Add New Contact")
+            print("2. Edit Contact")
+            print("3. Delete Contact")
+            print("4. Display Contacts")
+            print("5. Exit to Main Menu")
+
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                self.add_contact_from_console(address_book)
+            elif choice == "2":
+                self.edit_contact_from_console(address_book)
+            elif choice == "3":
+                self.delete_contact_main(address_book)
+            elif choice == "4":
+                address_book.display_contacts()
+            elif choice == "5":
+                break
+            else:
+                print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
     address_main = AddressBookMain()
     address_main.run()
