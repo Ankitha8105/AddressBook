c15cc3e72475fb89a0dbf90f6055a56f9318379a
Ankitha8105 <ankithabl2001@gmail.com>
2024-12-16 17:45:01 +0530

    [Ankitha] Added:Python program to view Persons by city or state

--- address_book.py
+++ address_book.py
@@ -18,6 +18,8 @@ class Contact:
 class AddressBook:
     def __init__(self):
         self.contacts = {}
+        self.city_dict = {}
+        self.state_dict = {}
 
     def add_contact(self, contact):
         key = f"{contact.first_name} {contact.last_name}".lower()
@@ -25,6 +27,15 @@ class AddressBook:
             self.contacts[key] = contact
             log.info(f"Contact {key} added successfully.")
             print(f"Contact {contact.first_name} {contact.last_name} added successfully.")
+
+            if contact.city.lower() not in self.city_dict:
+                self.city_dict[contact.city.lower()] = []
+            self.city_dict[contact.city.lower()].append(contact)
+
+            if contact.state.lower() not in self.state_dict:
+                self.state_dict[contact.state.lower()] = []
+            self.state_dict[contact.state.lower()].append(contact)
+
         else:
             log.info(f"Contact {key} already exists in the address book.")
             print(f"Contact {contact.first_name} {contact.last_name} already exists in the address book.")
@@ -48,19 +59,36 @@ class AddressBook:
     def delete_contact(self, first_name, last_name):
         key = f"{first_name} {last_name}".lower()
         if key in self.contacts:
+            contact = self.contacts[key]
             del self.contacts[key]
             log.info(f"Contact {key} deleted successfully.")
             print(f"Contact {first_name} {last_name} deleted successfully.")
+
+            if contact.city.lower() in self.city_dict:
+                self.city_dict[contact.city.lower()].remove(contact)
+                if not self.city_dict[contact.city.lower()]:
+                    del self.city_dict[contact.city.lower()]
+
+            if contact.state.lower() in self.state_dict:
+                self.state_dict[contact.state.lower()].remove(contact)
+                if not self.state_dict[contact.state.lower()]:
+                    del self.state_dict[contact.state.lower()]
         else:
             log.info(f"Contact {key} not found in the address book.")
             print(f"Contact {first_name} {last_name} not found in the address book.")
             
     def search_by_city_or_state(self, city_name=None, state_name=None):
         found_contacts = []
-        for contact in self.contacts.values():
-            if (city_name and contact.city.lower() == city_name.lower()) or \
-               (state_name and contact.state.lower() == state_name.lower()):
-                found_contacts.append(contact)
+        if city_name:
+            city_name = city_name.lower()
+            if city_name in self.city_dict:
+                found_contacts = self.city_dict[city_name]
+        
+        if state_name:
+            state_name = state_name.lower()
+            if state_name in self.state_dict:
+                found_contacts += self.state_dict[state_name]
+        
         return found_contacts
 
     def display_contacts(self):
